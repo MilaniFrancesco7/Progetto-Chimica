89b2f5be85f7ded3d7103869400e0f138e9c47a0
RealMasterZen <marcozen00@gmail.com>
2020-04-14 17:03:33 +0200

    Re-upload BackEnd code

--- BackEnd/funzioni.php
+++ BackEnd/funzioni.php
@@ -1,5 +1,4 @@
 <?php
-// Login & Inserimento Reagenti, Apparecchiatura e Vetreria
 
     // LOGIN FUNCTION
     $username = $_POST["username"];
@@ -18,6 +17,85 @@
     else
     {
         header("location: login.php"); //Va aggiunto il controllo della variabile di sessione
+    }
+
+?>
+
+<?php
+
+    // INSERIMENTO REAGENTE
+
+    $nome = $_POST["nome_reagente"];
+    $formula = $_POST["formula"];
+    $stato = $_POST["stato"];
+    $ditta = $_POST["ditta"];
+    $pittogramma = $_POST["pittogramma"];
+    $frase = $_POST["frase"];
+    $id_scheda_sicurezza = $_POST["scheda"];
+    $id_quantita = $_POST["quantita"];
+    $data_scadenza = $_POST["data_scadenza"];
+    $id_collocazione = $_POST["collocazione"];
+
+    $connect = mysqli_connect("localhost", "root", "XXXX", "Scuola");
+
+    $query = "INSERT INTO reagente (nome, formula, stato, ditta, pittogramma, frase, id_scheda_sicurezza, id_quantita, data_scadenza, id_collocazione)
+              VALUES ('$nome', '$formula', '$stato', '$ditta', '$pittogramma', '$frase', '$id_scheda_sicurezza', '$id_quantita', '$data_scadenza', '$id_collocazione')";
+
+    $result = mysqli_query($connect, $query);
+
+    if (mysql_num_rows($result) != 0)
+    {
+        echo "Elemento inserito con successo !";
+        header("location: index.php");
+    }
+
+?>
+
+<?php
+
+    // INSERIMENTO APPARECCHIATURA
+
+    $tipo = $_POST["tipo"];
+    $caratteristiche = $_POST["caratteristiche"];
+    $numero_inventario = $_POST["numero"];
+    $quantita = $_POST["quantita"];
+    $manuale = $_POST["manuale"];
+    $collocazione = $_POST["collocazione"];
+
+    $connect = mysqli_connect("localhost", "root", "XXXX", "Scuola");
+
+    $query = "INSERT INTO strumentaqzione_apparecchiatura (tipo, caratteristiche_tecniche, numero_inventario, id_quantita, id_manuale, id_collocazione)
+              VALUES ('$tipo', '$caratteristiche', '$numero_inventario', '$quantita', '$manuale', '$collocazione')";
+
+    $result = mysqli_query($connect, $query);
+
+    if (mysql_num_rows($result) != 0)
+    {
+        echo "Elemento inserito con successo !";
+        header("location: index.php");
+    }
+
+?>
+
+<?php
+
+    // INSERIMENTO VETRERIA
+
+    $tipo = $_POST["tipo"];
+    $quantita = $_POST["quantita"];
+    $collocazione = $_POST["collocazione"];
+
+    $connect = mysqli_connect("localhost", "root", "XXXX", "Scuola");
+
+    $query = "INSERT INTO strumentaqzione_apparecchiatura (tipo, quantita, id_collocazione)
+              VALUES ('$tipo', '$quantita', '$collocazione')";
+
+    $result = mysqli_query($connect, $query);
+
+    if (mysql_num_rows($result) != 0)
+    {
+        echo "Elemento inserito con successo !";
+        header("location: index.php");
     }
 
 ?>
